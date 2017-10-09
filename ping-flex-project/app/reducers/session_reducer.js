d50e3c0ee0817690df43e2d0693e9e7ae4523a34
Thomas Kilmer <tkilmer95@gmail.com>
2017-10-08 22:16:53 -0700

    Fix receive currentUser reducer

--- ping-flex-project/app/reducers/session_reducer.js
+++ ping-flex-project/app/reducers/session_reducer.js
@@ -6,8 +6,7 @@ const SessionReducer = (state = initialState, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      const newState = Object.assign({}, state, {currentUser: action.currentUser});
-      return newState;
+      return action.currentUser;
     default:
       return state;
   }
