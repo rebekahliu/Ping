9c4870140c81b4fd645fd8dab9ce9995daa60d9e
Betty <b.lam4392@gmail.com>
2017-10-11 19:00:27 -0700

    Create approve friend action

--- ping-flex-project/app/actions/add_friend_actions.js
+++ ping-flex-project/app/actions/add_friend_actions.js
@@ -1,6 +1,6 @@
 import { receiveErrors } from './ping_actions';
 export const RECEIVE_FRIEND_REQUEST = 'RECEIVE_FRIEND_REQUEST';
-export const RECEIVE_FRIEND_APPROVE = 'RECEIVE_FRIEND_APPROVE';
+// export const RECEIVE_FRIEND_APPROVE = 'RECEIVE_FRIEND_APPROVE';
 
 import AddFriendAPI from '../util/add_friend_api_util';
 
@@ -9,14 +9,24 @@ export const receiveFriendRequest = (currentUser) => ({
   currentUser
 });
 
-export const receiveFriendApprove = (currentUser) => ({
-  type: RECEIVE_FRIEND_APPROVE,
-  currentUser
-});
+// export const receiveFriendApprove = (currentUser) => ({
+//   type: RECEIVE_FRIEND_APPROVE,
+//   currentUser
+// });
 
 export const requestFriend = (token, friendId) => (dispatch) => {
   return (
     AddFriendAPI.requestFriend(token, friendId)
+      .then(
+        (currentUser) => dispatch(receiveFriendRequest(currentUser)),
+        (error) => dispatch(receiveErrors(error))
+      )
+  );
+};
+
+export const approveFriend = (token, friendId) => (dispatch) => {
+  return (
+    AddFriendAPI.approveFriend(token, friendId)
       .then(
         (currentUser) => dispatch(receiveFriendRequest(currentUser)),
         (error) => dispatch(receiveErrors(error))
