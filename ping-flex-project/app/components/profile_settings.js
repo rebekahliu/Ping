dd046302b63eaffb6c2e74c3be8a57b2b4ca2631
Thomas Kilmer <tkilmer95@gmail.com>
2017-10-14 23:59:22 -0700

    Style profile settings

--- ping-flex-project/app/components/profile_settings.js
+++ ping-flex-project/app/components/profile_settings.js
@@ -19,7 +19,7 @@ import {
 import API from '../../api';
 import * as SessionActions from '../actions/session_actions';
 
-import {icons} from '../../assets/icons';
+// import {icons} from '../../assets/icons';
 
 
 class ProfileSettings extends React.Component {
@@ -39,7 +39,7 @@ class ProfileSettings extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
       title: 'Settings',
-      headerBackTitle: null,
+      headerLeft: null,
       headerRight: <Button title="Done" onPress={() => {navigation.state.params.handleSave(navigation)}} />,
   };};
 
@@ -62,6 +62,29 @@ class ProfileSettings extends React.Component {
 
 
   render() {
+
+    const pingMessages = {
+      'default': "",
+      none: "",
+      home: "Hey, are you home?",
+      food: "Hey, want to get food?",
+      emergency: "Are you ok?",
+      school: "Hey, are you at school?",
+      work: "Hey, are you at work?",
+      drinks: "Hey, want to get drinks?",
+      coffee: "Hey, want to grab coffee?",
+    };
+
+    const icons = {
+      'home'  : require('../../assets/icons/homePing.png'),
+      'emergency'    : require('../../assets/icons/emergencyPing.png'),
+      'food'    : require('../../assets/icons/foodPing.png'),
+      'work'    : require('../../assets/icons/workPing.png'),
+      'drinks'    : require('../../assets/icons/drinksPing.png'),
+      'school'    : require('../../assets/icons/schoolPing.png'),
+      'coffee'    : require('../../assets/icons/coffeePing.png'),
+    }
+
     const name = this.props.currentUser.name;
     const customIcon1 = icons[this.state.customPing1];
     const customIcon2 = icons[this.state.customPing2];
@@ -69,33 +92,42 @@ class ProfileSettings extends React.Component {
 
     return(
       <View style={styles.container}>
-        <Text>Settings for {name}</Text>
-        <Slider
-          style={styles.slider}
-          minimumValue={0}
-          maximumValue={100}
-          step={1}
-          value={this.state.visibleRadius}
-          onValueChange={(value) => this.setState({visibleRadius: value})}
+        <Text style={styles.header}>DISCOVERY SETTINGS</Text>
+        <View style={styles.visibleRadius}>
+          <View style={styles.separatedTexts}>
+            <Text>Visible Radius</Text>
+            <Text>{this.state.visibleRadius}mi.</Text>
+          </View>
+          <Slider
+            style={styles.slider}
+            minimumValue={0}
+            maximumValue={100}
+            step={1}
+            value={this.state.visibleRadius}
+            onValueChange={(value) => this.setState({visibleRadius: value})}
+            />
 
-          />
-          <Text>
-            visible Radius {this.state.visibleRadius}mi.
-          </Text>
+        </View>
+        <Text style={styles.description}>If friends are outside your visibility radius, they won't be able to ping you</Text>
+        <View style={styles.showMe}>
+          <Text>Show me on Ping </Text>
           <Switch
             style={styles.switch}
             value={this.state.findable}
             onValueChange={(value) => this.setState({findable: value})}
             />
-          <Text>Show me on Ping </Text>
-          <Text>While turned off, your friends will not be able to get your location. Must be turned on to send pings</Text>
+        </View>
 
+        <Text style={styles.description}>While turned off, your friends will not be able to get your location. Must be turned on to send pings</Text>
+          <Text style={styles.header2}>CUSTOM PINGS</Text>
+        <Text style={styles.description}>Customize your ping shortcuts to add a short message when you ping your friends</Text>
+        <View style={styles.customPings}>
+          <View style={styles.customPingItem}>
             <Picker
               style={[styles.picker]} itemStyle={styles.onePickerItem}
               selectedValue={this.state.customPing1}
               onValueChange={(itemValue) => this.setState({customPing1: itemValue})}
-            >
-              <Picker.Item label="None" value="none" />
+              >
               <Picker.Item label="Home" value="home" />
               <Picker.Item label="Work" value="work" />
               <Picker.Item label="School" value="school" />
@@ -104,17 +136,18 @@ class ProfileSettings extends React.Component {
               <Picker.Item label="Drinks" value="drinks" />
               <Picker.Item label="Coffee" value="coffee" />
             </Picker>
+            <Text>{pingMessages[this.state.customPing1]}</Text>
             <Image
-               style={{width: 25, height: 25}}
-               source={customIcon1}
-             />
-
+              style={{width: 25, height: 25}}
+              source={customIcon1}
+              />
+          </View>
+          <View style={styles.customPingItem}>
             <Picker
               style={[styles.picker]} itemStyle={styles.onePickerItem}
               selectedValue={this.state.customPing2}
               onValueChange={(itemValue) => this.setState({customPing2: itemValue})}
-            >
-              <Picker.Item label="None" value="none" />
+              >
               <Picker.Item label="Home" value="home" />
               <Picker.Item label="Work" value="work" />
               <Picker.Item label="School" value="school" />
@@ -123,17 +156,18 @@ class ProfileSettings extends React.Component {
               <Picker.Item label="Drinks" value="drinks" />
               <Picker.Item label="Coffee" value="coffee" />
             </Picker>
+              <Text>{pingMessages[this.state.customPing2]}</Text>
             <Image
-               style={{width: 25, height: 25}}
-               source={customIcon2}
-             />
-
+              style={{width: 25, height: 25}}
+              source={customIcon2}
+              />
+          </View>
+          <View style={[styles.customPingItem, styles.lastPingItem]}>
             <Picker
               style={[styles.picker]} itemStyle={styles.onePickerItem}
               selectedValue={this.state.customPing3}
               onValueChange={(itemValue) => this.setState({customPing3: itemValue})}
-            >
-              <Picker.Item label="None" value="none" />
+              >
               <Picker.Item label="Home" value="home" />
               <Picker.Item label="Work" value="work" />
               <Picker.Item label="School" value="school" />
@@ -142,10 +176,13 @@ class ProfileSettings extends React.Component {
               <Picker.Item label="Drinks" value="drinks" />
               <Picker.Item label="Coffee" value="coffee" />
             </Picker>
+              <Text>{pingMessages[this.state.customPing3]}</Text>
             <Image
-               style={{width: 25, height: 25}}
-               source={customIcon3}
-             />
+              style={{width: 25, height: 25}}
+              source={customIcon3}
+              />
+          </View>
+        </View>
       </View>
     );
   }
@@ -155,24 +192,101 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: '#f7f7f7',
   },
   slider: {
     height: 10,
-    width: 200,
+    width: 300,
     margin: 10,
   },
   picker: {
-    width: 200,
-    height: 44,
-    borderColor: 'black',
-    borderWidth: 1,
+    width: 120,
+    height: 88,
   },
   onePickerItem: {
-    height: 44,
-    color: 'red'
+    height: 88,
+    color: 'black',
   },
+  visibleRadius: {
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderColor: 'lightgray',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    marginBottom: 3,
+  },
+  separatedTexts: {
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  showMe: {
+    backgroundColor: 'white',
+    borderColor: 'lightgray',
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    marginTop: 5,
+    marginBottom: 3,
+  },
+  customPings: {
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderColor: 'lightgray',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  customPingItem: {
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderColor: 'lightgray',
+    borderBottomWidth: 1,
+  },
+  lastPingItem: {
+    borderBottomWidth: 0,
+  },
+  header: {
+    paddingLeft: 10,
+    marginBottom: 10,
+    fontSize: 14,
+    color: '#5b5b5b',
+  },
+  header2: {
+    paddingLeft: 10,
+    marginTop: 5,
+    marginBottom: 3,
+    fontSize: 14,
+    color: '#5b5b5b',
+  },
+  description: {
+    paddingLeft: 10,
+    marginBottom: 10,
+    fontSize: 14,
+    color: 'grey'
+  }
+
 });
+
+
+
 
 var mapStateToProps = (state) => {
   return {
@@ -186,5 +300,7 @@ var mapDispatchToProps = (dispatch) => {
     updateSettings: (token, settings) => dispatch(SessionActions.updateUser(token, settings))
   };
 }
+
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProfileSettings);
